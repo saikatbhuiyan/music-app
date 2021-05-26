2185ecd241b455cb0d1b14d82e6dc787216df929
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-05-25 21:29:44 -0700

    user register with firebase

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -82,7 +82,6 @@
 </template>
 
 <script>
-import { auth, db } from '@/includes/firebase';
 
 export default {
   name: 'RegisterForm',
@@ -113,21 +112,18 @@ export default {
       this.reg_alert_variant = 'bg-blue-500';
       this.reg_alert_msg = 'Please wait! Your account is being created.';
 
-      let userCreate = null
       try {
-        userCreate = await auth.createUserWithEmailAndPassword(
-          values.email, values.password,
-        ); 
+        await this.$store.dispatch('register', values);
       } catch (error) {
         this.reg_in_submission = false;
         this.reg_alert_variant = 'bg-red-500';
-        this.reg_alert_msg = 'An unexpected error occoured. Please try again later.';
-        return
+        this.reg_alert_msg = 'An unexpected error occured. Please try again later.';
+        return;
       }
 
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'Success! Your account has been created.';
-      console.log(values);
+      window.location.reload();
     },
   },
 };
