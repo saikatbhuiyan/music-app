91ab7310f25fd02890cdc1132fdb90900589f0cd
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-09-01 23:37:06 +0600

    add route

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
+import Manage from '@/views/Manage.vue';
+
 
 const routes = [
-
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/about',
+    component: About,
+  },
+  {
+    path: '/manage',
+    component: Manage,
+  }
 ];
 
 const router = createRouter({
