b60f63ef4a68daa91dda96e60734486a70e0f0f4
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-09-10 12:55:44 +0600

    update route with meta

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -43,7 +43,7 @@ export default {
     ...mapMutations(["toggleAuthModal"]),
     signout() {
       this.$store.dispatch('signout');
-      if (this.$route.name === 'manage') {
+      if (this.$route.meta.requiresAuth) {
         this.$router.push({ name: 'home' });
       }
     },
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
 import Manage from '@/views/Manage.vue';
-
+import store from '@/store';
 
 const routes = [
   {
@@ -19,6 +19,9 @@ const routes = [
     name: 'manage',
     // alias: '/manage', // alternative link
     path: '/manage-music',
+    meta: { 
+      requiresAuth: true,
+    },
     component: Manage,
   },
   {
@@ -36,5 +39,18 @@ const router = createRouter({
   routes,
   linkExactActiveClass: 'text-yellow-500',
 });
+
+router.beforeEach((to, from, next) => {
+  // console.log(to.matched)
+  if (!to.matched.some((record) => record.meta.requiresAuth)) {
+    next();
+    return;
+  }
+  if (store.state.userLoggedIn) {
+    next();
+  } else {
+    next({ name: 'home' });
+  }
+})
 
 export default router;
