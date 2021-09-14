2e720e94e8850dac29a097dc2478ba0a50d9ccab
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-09-15 01:04:28 +0600

    drag and drop hover effict

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -10,6 +10,14 @@
         class="w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed
                 border-gray-400 text-gray-400 transition duration-500 hover:text-white
                 hover:bg-green-400 hover:border-green-400 hover:border-solid"
+        :class="{ 'bg-green-400 border-green-400 border-solid': is_dragover }"
+        @drag.prevent.stop=""
+        @dragstart.prevent.stop=""
+        @dragend.prevent.stop="is_dragover = false"
+        @dragover.prevent.stop="is_dragover = true"
+        @dragenter.prevent.stop="is_dragover = true"
+        @dragleave.prevent.stop="is_dragover = false"
+        @drop.prevent.stop="upload"
       >
         <h5>Drop your files here</h5>
       </div>
@@ -41,6 +49,16 @@
 
 <script>
 export default {
-    name: "Upload"
+  name: "Upload",
+  data() {
+    return {
+      is_dragover: false
+    };
+  },
+  methods: {
+    upload() {
+        this.is_dragover = false;
+    }
+  }
 };
 </script>
