61d9bdb5d040107fdeb450720a48949227cfac6f
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-11-08 07:46:08 +0600

    mp3 file upload to firebase

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import 'firebase/storage';
 
 var firebaseConfig = {
   apiKey: "AIzaSyBzPfHORZM_oPzG2Qoah1i-rq1_COvJQoY",
@@ -15,7 +16,8 @@ export default firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection("users");
 
-export { auth, db, usersCollection };
+export { auth, db, usersCollection, storage };
